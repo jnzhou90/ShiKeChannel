d70195e8be6e8a99384a19820e28ab95f2c26660
jnzhou <jnzhou90@sina.com>
2014-11-24 11:36:40 +0800

    修改学霸养成记的-我要当学员的布局和ListView在一个刷新界面上。

--- src/com/example/shikechannel/ChannelActivity.java
+++ src/com/example/shikechannel/ChannelActivity.java
@@ -15,6 +15,7 @@ import org.json.JSONArray;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,35 +28,19 @@ public class ChannelActivity extends Activity {
 	private List<Map<String, Object>> listItems;
 	private ChannelListViewAdapter channelListViewAdapter;
 
-	// private Integer[] user_pic = { R.drawable.kaibiedetanke,
-	// R.drawable.sunjin,
-	// R.drawable.annan, R.drawable.ic_launcher };
-	// private String[] user_nickname = { "开贝塔的坦克", "孙锦", "安南", "周江南1" };
-	// private String[] release_time = { "1分钟前", "1分钟前", "1分钟前", "3分钟前" };
-	// private String[] content = { "每天临睡觉前背单词！！", " 俞敏洪讲座", "每天临睡觉前背单词",
-	// "每天早上读书学习" };
-	// private String[] remind_time = { "每天  6:00", "每天  6:00", "每天  6:00",
-	// "每天  6:00" };
-	// private String[] fans_count = { "643人关注", "643人关注", "643人关注", "643人关注" };
-	// private String[] record_count = { "1165条记录", "1165条记录", "1165条记录",
-	// "1165条记录" };
-
-	// private TextView channelDetailTV =
-	// (TextView)findViewById(R.id.tv_channel_details);
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.activity_channel);
 		setContentView(R.layout.activity_main);
-		// final ImageButton newButton = (ImageButton)
-		// findViewById(R.id.newrecord);
+		
 
 		listView = (ListView) findViewById(R.id.channel_ListView);
 		listItems = getListItems();
-
 		channelListViewAdapter = new ChannelListViewAdapter(this, listItems);
-
+        //添加listView的头部
+		View headView =LayoutInflater.from(this).inflate(R.layout.channel_themes, null);
+		listView.addHeaderView(headView);
 		listView.setAdapter(channelListViewAdapter);
 	}
 
@@ -100,20 +85,6 @@ public class ChannelActivity extends Activity {
 
 	}
 
-	/*
-	 * private List<Map<String, Object>> getListItems() {
-	 * 
-	 * List<Map<String, Object>> listItems = new ArrayList<Map<String,
-	 * Object>>();
-	 * 
-	 * for (int i = 0; i < user_nickname.length; i++) { Map<String, Object> map
-	 * = new HashMap<String, Object>(); map.put("user_pic", user_pic[i]);
-	 * map.put("user_nickname", user_nickname[i]); map.put("release_time",
-	 * release_time[i]); map.put("content", content[i]); map.put("remind_time",
-	 * remind_time[i]); map.put("fans_count", fans_count[i]);
-	 * map.put("record_count", record_count[i]); listItems.add(map); } return
-	 * listItems; }
-	 */
 	private List<Map<String, Object>> getListItems() {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -121,8 +92,6 @@ public class ChannelActivity extends Activity {
 		try {
 			inputStream = this.getAssets().open("Channel_pic.txt");
 			String json = readTextFile(inputStream);
-			//String jsonUTF8 = new String(json);
-			//String jsonUTF8 = JsonFilter(json);
 			
 			JSONArray array = new JSONArray(json);
 			for (int i = 0; i < array.length(); i++) {
@@ -180,15 +149,6 @@ public class ChannelActivity extends Activity {
 	
 	
 	
-/*    public static String toUtf8(String str) { 
-        try {
-			return new String(str.getBytes("UTF-8"),"UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return str;
-  }*/
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
