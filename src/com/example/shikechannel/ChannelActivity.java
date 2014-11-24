18fe4e44b03f631cf5233e2c38bd9f493b8de8dc
jnzhou <jnzhou90@sina.com>
2014-11-24 15:22:16 +0800

    添加刷新功能，没有发现错误，但是方向apk在手机上运行不了

--- src/com/example/shikechannel/ChannelActivity.java
+++ src/com/example/shikechannel/ChannelActivity.java
@@ -12,9 +12,17 @@ import java.util.Map;
 
 import org.json.JSONArray;
 
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,23 +33,62 @@ import android.widget.Toast;
 public class ChannelActivity extends Activity {
 
 	private ListView listView;
-	private List<Map<String, Object>> listItems;
-	private ChannelListViewAdapter channelListViewAdapter;
-
+	private List<Map<String, Object>> listItems; // 显示的列表对应的字符串
+	private ChannelListViewAdapter channelListViewAdapter; // ListView的适配器
+	private PullToRefreshListView mPullRefreshListView;// PullToRefreshListView实例
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 
-		listView = (ListView) findViewById(R.id.channel_ListView);
+		// 初始化mPullRefreshListView并设置监听器，以执行当需要刷新时，
+		// 应该怎么办，至于真正执行刷新的类GetDataTask（）
+
+		mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_listview);
+
+		// Set a listener to be invoked when the list should be refreshed.
+		mPullRefreshListView
+				.setOnRefreshListener(new OnRefreshListener<ListView>() {
+					@Override
+					public void onRefresh(
+							PullToRefreshBase<ListView> refreshView) {
+						String label = DateUtils.formatDateTime(
+								getApplicationContext(),
+								System.currentTimeMillis(),
+								DateUtils.FORMAT_SHOW_TIME
+										| DateUtils.FORMAT_SHOW_DATE
+										| DateUtils.FORMAT_ABBREV_ALL);
+
+						// Update the LastUpdatedLabel
+						refreshView.getLoadingLayoutProxy()
+								.setLastUpdatedLabel(label);
+
+						// Do work to refresh the list here.
+						new GetDataTask().execute();
+					}
+				});
+		mPullRefreshListView
+		.setOnLastItemVisibleListener(new OnLastItemVisibleListener() {
+
+			@Override
+			public void onLastItemVisible() {
+				// TODO Auto-generated method stub
+				Toast.makeText(ChannelActivity.this, "End of List!",
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		View headView = LayoutInflater.from(this).inflate(
+				R.layout.channel_themes, null);
 		listItems = getListItems();
 		channelListViewAdapter = new ChannelListViewAdapter(this, listItems);
-        //添加listView的头部
-		View headView =LayoutInflater.from(this).inflate(R.layout.channel_themes, null);
-		listView.addHeaderView(headView);
+		// listView = (ListView) findViewById(R.id.channel_ListView);
+		listView = mPullRefreshListView.getRefreshableView();
 		listView.setAdapter(channelListViewAdapter);
+		
+		// 添加listView的头		
+		listView.addHeaderView(headView);
+	
 	}
 
 	/**
@@ -60,8 +107,8 @@ public class ChannelActivity extends Activity {
 	 */
 	public void clickBack(View view) {
 		Toast.makeText(ChannelActivity.this, "亲， 这是返回键", 1000).show();
-		//Toast.makeText(null, ACTIVITY_SERVICE, duration)
-		//ChannelActivity.this.finish();
+		// Toast.makeText(null, ACTIVITY_SERVICE, duration)
+		// ChannelActivity.this.finish();
 	}
 
 	/**
@@ -92,7 +139,7 @@ public class ChannelActivity extends Activity {
 		try {
 			inputStream = this.getAssets().open("Channel_pic.txt");
 			String json = readTextFile(inputStream);
-			
+
 			JSONArray array = new JSONArray(json);
 			for (int i = 0; i < array.length(); i++) {
 				map = new HashMap<String, Object>();
@@ -146,9 +193,53 @@ public class ChannelActivity extends Activity {
 
 		return readedStr;
 	}
-	
-	
-	
+
+	private class GetDataTask extends
+			AsyncTask<Void, Void, HashMap<String, Object>> {
+
+		@Override
+		protected HashMap<String, Object> doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			// Simulates a background job.
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			try {
+			
+				map = new HashMap<String, Object>();
+				map.put("user_pic", R.drawable.annan);
+				map.put("user_nickname", "下拉后更新后的名字");
+				map.put("release_time", "30秒前");
+				map.put("content", "下拉后更新后的内容");
+				map.put("remind_time", "下拉刷新后的时间");
+				map.put("fans_count", "1000");
+				map.put("record_count", "16754");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				setTitle("map出错了");
+			}
+
+			return map;
+		}
+
+		// 这里是对刷新的响应，可以利用addFirst（）和addLast()函数将新加的内容加到LISTView中
+		// 根据AsyncTask的原理，onPostExecute里的result的值就是doInBackground()的返回值
+		@Override
+		protected void onPostExecute(HashMap<String, Object> result) {
+			// 在头部增加新添内容
+			listItems.add(0, result);
+
+			// 通知程序数据集已经改变，如果不做通知，那么将不会刷新mListItems的集合
+			channelListViewAdapter.notifyDataSetChanged();
+			// Call onRefreshComplete when the list has been refreshed.
+			mPullRefreshListView.onRefreshComplete();
+
+			super.onPostExecute(result);// 这句是必有的，AsyncTask规定的格式
+		}
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
